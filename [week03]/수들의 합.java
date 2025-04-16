e94d8526a456a9ea5bc6542fac4f679322a25561
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-04-16 21:40:07 +0900

    Create 수들의 합.java

---
+++ [week03]/수들의 합.java
@@ -0,0 +1,27 @@
+package KB_ITL_CT;
+import java.util.*;
+public class 수들의합 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int start = 1,end=1;
+		int answer=1;
+		int sum = 1;
+		while(true) {
+			if(end==N)break;
+			if(sum<N) {
+				end+=1;
+				sum+=end;
+			}else if(sum>N) {
+				sum-=start;
+				start+=1;
+			}else if(sum==N){
+				answer+=1;
+				end+=1;
+				sum+=end;
+			}
+		}
+		System.out.println(answer);
+		sc.close();
+	}
+}
