920e037094d9cd2e2484d019a9b6d2b6188ab9d3
eujeong <126744281+zlwmxkdla@users.noreply.github.com>
2025-06-19 00:29:48 +0900

    Create k번째수.java

---
+++ [week12]/k번째수.java
@@ -0,0 +1,27 @@
+package KB_ITL;
+import java.io.*;
+public class w12_K번째수 {
+    public static void main(String[]args)throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int K = Integer.parseInt(bf.readLine());
+
+        long start = 1;
+        long end = K;
+
+        while(start<end){
+            long mid = (start+end)/2;
+            long count = 0;
+
+            for(int i=1;i<=N;i++){
+                count += Math.min(mid/i,N);
+            }
+            if(K<=count){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        System.out.println(start);
+    }
+}
